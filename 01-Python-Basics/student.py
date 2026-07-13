2a72a9962330207ad128c5ec7da98a76df1019e5
naveengouda091 <bevinamaradnandishgouda@gmail.com>
2026-07-13 14:02:51 +0530

    dict

--- 01-Python-Basics/student.py
+++ 01-Python-Basics/student.py
@@ -1,12 +1,28 @@
+# def main():
+#     student = get_student()
+#     print(f"{student[0]} from {student[1]}")
+
+
+# def get_student():
+#     name = input("name: ")
+#     house = input("house: ")
+#     return (name, house)#tuple is immutable, so we can use it to return multiple values from a function. We can also use a list or a dictionary, but a tuple is more appropriate here since we don't need to modify the values.
+
+
+# if __name__ == "__main__":
+#     main()
+
+
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
+    print(f"{student['name']} from {student['house']}")
 
 
 def get_student():
-    name = input("name: ")
-    house = input("house: ")
-    return (name, house)#tuple is immutable, so we can use it to return multiple values from a function. We can also use a list or a dictionary, but a tuple is more appropriate here since we don't need to modify the values.
+    student = {}
+    student["name"] = input("name: ")
+    student["house"] = input("house: ")
+    return student
 
 
 if __name__ == "__main__":
