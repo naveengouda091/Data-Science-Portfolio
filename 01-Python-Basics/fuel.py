f3a4997c3a708da0c3fefcb35f30df7b7e4af39a
naveengouda091 <bevinamaradnandishgouda@gmail.com>
2026-08-20 04:26:42 +0530

    fuel course

---
+++ 01-Python-Basics/fuel.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number"))
+
+if a%3 == a%5:
+    print("hi hello")
+
+elif a%3 == 0:
+    print("hello")
+
+elif a%5 == 0:
+    print("hi")
--- 01-Python-Basics/say.py
+++ 01-Python-Basics/say.py
@@ -3,4 +3,3 @@ import sys
 
 if len(sys.argv)==2:
     cowsay.cow("hello, "+ sys.argv[1])
-######
