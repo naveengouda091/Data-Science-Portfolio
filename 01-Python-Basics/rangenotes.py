83e3fdff86660dac589f5ef91e63d6691911acc7
naveengouda091 <bevinamaradnandishgouda@gmail.com>
2026-08-20 04:42:04 +0530

    range notes

---
+++ 01-Python-Basics/rangenotes.py
@@ -0,0 +1,27 @@
+'''
+range is ta method comes with three different formats. 
+
+range with one parameter --- it starts fromm zero till the given value minus 1(x-1)
+range with two parameters ---when we pass two parameters  first parameter to start the loop second parameter to end the loop. the end valuve will be -1 
+range with three parameters --- first parameter start second parameter end value third parameter incremnet or decrement 
+'''
+
+'''
+following examples demos loops with differ parameters
+'''
+
+# for i in range(5):
+#     print(i)
+
+# print("------------")
+# for x in range(2,10):
+#     print(x)
+
+# print("------------")
+# for y in range(10,0,-1):
+#     print(y)
+
+
+for i in range(0,11,2):
+    # if (i%2==0):
+     print(i)
