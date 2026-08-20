0114d448b42c38412957e07e7293666f27667f5c
naveengouda091 <bevinamaradnandishgouda@gmail.com>
2026-08-20 14:35:48 +0530

    practice

--- 01-Python-Basics/whileloop.py
+++ 01-Python-Basics/whileloop.py
@@ -17,9 +17,34 @@ while(condition):
 #     i=i+1
 
 
-fact=1
-i=int(input("enter a number"))
-while(i>0):
-    fact=fact*i
-    i=i-1
-print(fact)
+# fact=1
+# i=int(input("enter a number"))
+# while(i>0):
+#     fact=fact*i
+#     i=i-1
+# print(fact)
+
+
+'''
+write a program to read a number and display it indivusual digits example 
+246 =====  6 4 2
+write a program to read a number and find the sum of indivusual digits  
+
+
+write a pgm to read a num and reverse the same 
+
+write a prgm to read a number and check is it a panadrome number
+
+write a prgm to read a number an check is it a armstrong number 
+
+'''
+
+
+temp=0
+sum=0
+num=int(input("enter a number"))
+while(num>0):
+    temp=num%10
+    sum=sum+temp
+    num=num/10
+print(sum)
